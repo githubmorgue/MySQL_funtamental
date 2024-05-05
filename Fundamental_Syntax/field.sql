ae7ce370ba1d02a755245381135541a06ab8c7cf
7zZFHpYyu9 <jianguoh003@gmail.com>
2024-05-05 09:43:28 +0800

    Syntax

--- Fundamental_Syntax/field.sql
+++ Fundamental_Syntax/field.sql
@@ -1,3 +1,23 @@
 SELECT Concat(vend_name, ' (', vend_country, ')')
 FROM vendors
 ORDER BY vend_name;
+
+SELECT Concat(RTrim(vend_name), ' (', RTrim(vend_country), ')')
+FROM vendors
+ORDER BY vend_name;
+
+SELECT Concat(RTrim(vend_name), ' (', RTrim(vend_country), ')') AS vend_title
+FROM vendors
+ORDER BY vend_name;
+
+SELECT prod_id, quantity, item_price
+FROM orderitems
+WHERE order_num = 20005;
+
+SELECT prod_id,
+	   quantity,
+       item_price,
+       quantity*item_price AS expanded_price
+FROM orderitems
+WHERE order_num = 20005;
+
---
+++ Fundamental_Syntax/function.sql
@@ -0,0 +1,3 @@
+SELECT vend_name, upper(vend_name) AS vend_name_upcase
+FROM vendors
+ORDER BY vend_name;
